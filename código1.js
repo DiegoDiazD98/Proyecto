f911a16ce7866d801af34c11d480fa854abd3cc7
Diego Diaz <diego.idd98@gmail.com>
2025-02-26 14:14:07 -0300

    Primer commit

---
+++ código1.js
@@ -0,0 +1,7 @@
+function clave(palabra){
+    if (palabra == "2Fj(jjbFsuj" || "eoZiugBf&g9"){
+        console.log("contraseña correcta");
+    }
+    else {console.log("contraseña incorrecta")}
+}
+console.log(clave("2Fj(jjb"))
